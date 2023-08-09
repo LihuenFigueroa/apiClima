88426b54a35e78f8581bb7cbdb7b3ac987a7d179
LihuenFigueroa <lihuenfigueroa@gmail.com>
2023-08-09 17:53:07 -0300

    Call to External Weather Api tested

--- src/main/java/com/clima/apiClima/controllers/ClimaController.java
+++ src/main/java/com/clima/apiClima/controllers/ClimaController.java
@@ -3,7 +3,12 @@ package com.clima.apiClima.controllers;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import javax.print.attribute.standard.MediaTray;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.clima.apiClima.models.Clima;
 import com.clima.apiClima.services.ClimaService;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 @RestController
 @RequestMapping("/clima")
@@ -51,6 +58,14 @@ public class ClimaController {
         } else {
             return "No se pudo eliminar clima con id : " + id;
         }
+    }
+
+    @GetMapping(value = "/getForecast", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getForeCast() throws JSONException {
+        String uri = "http://dataservice.accuweather.com/currentconditions/v1/349727?apikey=WTcU6za9qLVZh7ZqSkseHQYsWdbABtQh";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
     }
 
 }
